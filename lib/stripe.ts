e8d1eb47481e1267650d0230fd9bbac996df4cba
Ezra Haugabrooks <ezra@aymscores.com>
2025-08-17 21:55:11 -0400

    Remove all environment variable dependencies for Vercel build

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -33,8 +33,8 @@ export const createCheckoutSession = async (amount: number, metadata: any) => {
         },
       ],
       mode: 'payment',
-      success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/cancel`,
+      success_url: `https://example.com/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `https://example.com/cancel`,
       metadata,
     })
 
